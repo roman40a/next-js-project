5ecde00e4dfa0a7a316703a1bec4ac4c631ff93a
rsoroka <rsoroa@luxoft.com>
2019-11-27 13:48:42 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NextPage } from 'next';
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+    <h1>Hello world! - user agent: {userAgent}</h1>
+);
+
+Home.getInitialProps = async (context) => {
+    const { req } = context;
+    const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+    return { userAgent };
+};
+
+export default Home;
