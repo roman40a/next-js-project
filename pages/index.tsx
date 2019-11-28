a754432f613cb0ca29fac9a836f080dafc07cde4
rsoroka <rsoroa@luxoft.com>
2019-11-28 12:15:18 +0300

    Change localhost domain

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,16 @@ import { NextPage } from 'next';
 import fetch from 'isomorphic-unfetch';
 import { Home } from '../components/home/home.component';
 
-const HomePage: NextPage<{ userAgent: string; users: { id: string }[] }> = ({
-    userAgent,
-    users,
-}) => <Home users={users} header={userAgent} />;
+const HomePage: NextPage<{ userAgent: string; users: { id: string }[] }> = ({ userAgent, users }) => (
+	<Home users={users} header={userAgent} />
+);
 
 HomePage.getInitialProps = async context => {
-    const { req } = context;
-    const response = await fetch('http://localhost:3000/api/users');
-    const users: { id: string }[] = await response.json();
-    const userAgent = req
-        ? req.headers['user-agent'] || ''
-        : navigator.userAgent;
-    return { userAgent, users };
+	const { req } = context;
+	const response = await fetch('http://127.0.0.1:3000/api/users');
+	const users: { id: string }[] = await response.json();
+	const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+	return { userAgent, users };
 };
 
 export default HomePage;
